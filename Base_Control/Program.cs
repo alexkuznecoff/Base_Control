6149b20e870856c851e08d96523a36d51fd76295
a.kuznecoff82@gmail.com <a.kuznecoff82@gmail.com>
2020-01-22 16:57:08 +0200

    Commit

--- Base_Control/Program.cs
+++ Base_Control/Program.cs
@@ -6,10 +6,75 @@ using System.Threading.Tasks;
 
 namespace Base_Control
 {
+    public struct City
+    {
+        public string Name { get; set; }
+        public int Populated { get; set; }
+        public int Area { get; set; }
+    }
     class Program
     {
+     
         static void Main(string[] args)
         {
+            string str = "Kharkiv = 1431000,350; Kiev = 2804000,839; Las Vegas = 603400,352";
+            string[] result = str.Split(';');
+            City[] citys = new City[result.Length];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                citys[i].Name = result[i].Split('=')[0];
+                citys[i].Populated = int.Parse(result[i].Split('=',',')[1]);
+                citys[i].Area = int.Parse(result[i].Split(',')[1]);
+            }
+            Console.WriteLine(TheMostPpulatedCity(citys));
+            Console.WriteLine(LongestName(citys));
+            PrintDesity(citys);
+            Console.ReadKey();
+        }
+
+        public static void PrintDesity(City[] citys)  
+        {
+            Console.WriteLine("\nDensity :");
+            Console.WriteLine();       
+            for (int i = 0; i < citys.Length; i++)
+            {
+                double density = citys[i].Populated / citys[i].Area;
+                Console.WriteLine($" {citys[i].Name} -  {density}");
+            }
+        }
+
+        public static string TheMostPpulatedCity(City[] citys)
+        {
+            City mostPopCity = new City();
+
+            foreach (var city in citys)
+            {
+                if (city.Populated > mostPopCity.Populated)  
+                {
+                    mostPopCity.Name = city.Name;
+                    mostPopCity.Populated = city.Populated; 
+                    mostPopCity.Area = city.Area;
+                }
+
+            }
+            return $"The mos populated city: {mostPopCity.Name} = {mostPopCity.Populated} , area =({mostPopCity.Area})";
+        }
+        public static string LongestName(City[] citys)
+        {
+            City longestName = new City();
+            longestName.Name ="";
+
+            foreach(var city in citys)
+            {
+                if(city.Name.Length > longestName.Name.Length)
+                {
+                    longestName.Name = city.Name;
+                    longestName.Populated = city.Populated;
+                    longestName.Area = city.Area;
+                }
+            }
+            return $"\nLongest name : {longestName.Name} ({longestName.Name.Length}) litters ";
         }
     }
 }
